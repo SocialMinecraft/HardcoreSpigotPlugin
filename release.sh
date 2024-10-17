924f04d42218c3038fe6a3e7f2cb32acb5830ca4
Dylan Garvis <dylan@garvis.dev>
2024-10-17 19:40:51 -0400

    ci: Added release script.

---
+++ release.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# to install the tool
+# npm i -g commit-and-tag-version
+# add --first-release if first release to not bump versions.
+commit-and-tag-version --packageFiles pom.xml --bumpFiles pom.xml
+
